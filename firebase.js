aa5dd2ecce27db7f6aecd7b2b1df1c0eeabcaa8d
tonytega <aakpovire@gmail.com>
2022-10-17 11:37:29 +0100

    i dont know what im doing

--- firebase.js
+++ firebase.js
@@ -1,15 +1,13 @@
-import { initializeApp } from "firebase-admin/app";
-import { getDatabase } from "firebase/database";
 import admin  from "firebase-admin";
 import {getAuth} from 'firebase-admin/auth'
 const apiKey = process.env.VITE_API_KEY;
-
+// const stringKey = JSON.parse(apiKey)
 // TODO: Replace the following with your app's Firebase project configuration
 // See: https://firebase.google.com/docs/web/learn-more#config-object
 
 // var admin = require("firebase-admin");
 import {serviceAccount} from './serviceAccount.js'; 
-
+// console.log(typeof(apiKey))
 const app = admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://chatapp-a8732-default-rtdb.firebaseio.com"
